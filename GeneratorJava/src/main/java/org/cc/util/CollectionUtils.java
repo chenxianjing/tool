78b2f4824940c44d0e152b94c98c1ae6484908c9
chenxianjing <1531517063@qq.com>
2018-10-30 18:40:53 +0800

    增加对jdk的api的集合的支持

--- GeneratorJava/src/main/java/org/cc/util/CollectionUtils.java
+++ GeneratorJava/src/main/java/org/cc/util/CollectionUtils.java
@@ -7,6 +7,43 @@ import java.util.Iterator;
 public class CollectionUtils {
 
 	/**
+	 * 交或者差集 集合的泛型为原始类型的包装类
+	 * 
+	 * @param a
+	 * @param b
+	 * @param field
+	 * @param flag
+	 *            false 差集 true 交集
+	 * @return
+	 */
+	private static <E> Collection<E> mix(Collection<E> a, Collection<E> b, boolean flag) {
+		Collection<E> a1 = a;
+		Collection<E> b1 = b;
+		Iterator<E> it = a1.iterator();
+		Iterator<E> it2 = b1.iterator();
+		E e1;
+		boolean contain = flag;
+		while (it.hasNext()) {
+			e1 = it.next();
+			contain = flag;
+			try {
+				while (it2.hasNext()) {
+					if (e1.equals(it2.next())) {
+						contain = !flag;
+						break;
+					}
+				}
+				if (contain) {
+					it.remove();
+				}
+			} catch (IllegalArgumentException | SecurityException e3) {
+				e3.printStackTrace();
+			}
+		}
+		return a1;
+	}
+
+	/**
 	 * 交或者差集
 	 * 
 	 * @param a
@@ -61,6 +98,21 @@ public class CollectionUtils {
 	 *            实体E的比较属性
 	 * @return
 	 */
+	public static <E> Collection<E> intersect(Collection<E> a, Collection<E> b) {
+		return mix(a, b, true);
+	}
+
+	/**
+	 * 取交集
+	 * 
+	 * @param a
+	 *            集合a
+	 * @param b
+	 *            集合b
+	 * @param field
+	 *            实体E的比较属性
+	 * @return
+	 */
 	public static <E> Collection<E> intersect(Collection<E> a, Collection<E> b, String field) {
 		return mix(a, b, field, true);
 	}
@@ -76,8 +128,61 @@ public class CollectionUtils {
 	 *            实体E的比较属性
 	 * @return
 	 */
+	public static <E> Collection<E> except(Collection<E> a, Collection<E> b) {
+		return mix(a, b, false);
+	}
+
+	/**
+	 * 取差集 a-b a减去公共部分剩下的部分
+	 * 
+	 * @param a
+	 *            集合a
+	 * @param b
+	 *            集合b
+	 * @param field
+	 *            实体E的比较属性
+	 * @return
+	 */
 	public static <E> Collection<E> except(Collection<E> a, Collection<E> b, String field) {
 		return mix(a, b, field, false);
+	}
+
+	/**
+	 * 取并集
+	 * 
+	 * @param a
+	 *            集合a
+	 * @param b
+	 *            集合b
+	 * @param field
+	 *            实体E的比较属性
+	 * @return
+	 */
+	public static <E> Collection<E> union(Collection<E> a, Collection<E> b) {
+		Collection<E> a1 = a;
+		Collection<E> b1 = b;
+		Iterator<E> it = a1.iterator();
+		Iterator<E> it2 = b1.iterator();
+		E e1;
+		boolean contain = false;
+		while (it.hasNext()) {
+			e1 = it.next();
+			contain = false;
+			try {
+				while (it2.hasNext()) {
+					if (e1.equals(it2.next())) {
+						contain = true;
+						break;
+					}
+				}
+				if (!contain) {
+					b.add(e1);
+				}
+			} catch (IllegalArgumentException | SecurityException e3) {
+				e3.printStackTrace();
+			}
+		}
+		return b1;
 	}
 
 	/**
